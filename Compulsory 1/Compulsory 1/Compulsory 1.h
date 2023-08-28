63a8b89a3b652bd22e93d93a5bfdde214fcb8d6f
Per Kristian Coskun <47058816+Zartok89@users.noreply.github.com>
2023-08-28 09:47:16 +0200

    Compulsory 1 complete

---
+++ Compulsory 1/Compulsory 1/Compulsory 1.h
@@ -0,0 +1,15 @@
+#pragma once
+
+double NumberInput;
+double FactorialResult;
+double FibonacciResult;
+
+//Calculating the factorial of n
+double Factorial(double n);
+
+//Calculating the fibonacci number of n
+double Fibonacci(double n);
+
+void PrintFactorial();
+
+void PrintFibonacci();
